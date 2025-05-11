91d51237f71a56149a85eed29bf55cfa2fcd33a5
Ayub Ali <pltsummary@gmail.com>
2025-05-11 07:09:27 -0600

    updated libaries

--- dosblocker.py
+++ dosblocker.py
@@ -1,6 +1,11 @@
-import os 
-import sys 
-import time
-import collections import defaultdict
-from scapy.all import sniff, IP
+import os
+import sys
+import time 
+from collections import defaultdict
+from scapy.all import sniff, IP 
 
+packet_count = {}
+
+packet_count["192.168.1.1"] += 1  # ❌ This gives an error!
+
+print(packet_count)
