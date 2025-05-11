086dc47376bbeae8baeb693561e998fad3f8cef0
Ayub Ali <pltsummary@gmail.com>
2025-05-11 08:03:04 -0600

    packet callback function

--- dosblocker.py
+++ dosblocker.py
@@ -4,8 +4,24 @@ import time
 from collections import defaultdict
 from scapy.all import sniff, IP 
 
-packet_count = {}
+THRESHOLD = 40
+print(f"THRESHOLD :{THRESHOLD}") #If ip sends more than 40 packets in 1 second we block it 
 
-packet_count["192.168.1.1"] += 1  # ❌ This gives an error!
+def packet_callback(packet): 
+    src_ip = packet[IP].src
+    packet_count[src_ip] += 1 
 
-print(packet_count)
+    current_time = time.time()
+    time_interval = current_time - start_time[0]
+
+    if time_interval >= 1:
+        for ip, count in packet_count.items():
+            packet_rate = count / time_interval
+            if packet_rate > THRESHOLD and ip not in blocked_ips:
+                print(f"Blocking IP: {ip}, packet rate: {packet_rate}")
+                os.system(f"iptables -A INPUT -s {ip} -j DROP")
+                blocked_ips.add(ip)
+
+        packet_count.clear()
+        start_time[0] = current_time
+
